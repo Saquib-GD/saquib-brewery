68201fa00dde2c4510eaa0a5c7bc9aade8e32fcc
Saquib-GD <mohammed.saquib1420@gmail.com>
2023-04-01 18:29:19 +0530

    Initial spring boot project

---
+++ src/main/java/com/brewery/SaquibBreweryApplication.java
@@ -0,0 +1,12 @@
+package com.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaquibBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaquibBreweryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/brewery/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.brewery.service.impl;
+
+import com.brewery.model.CustomerDto;
+import com.brewery.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name("Saquib")
+                .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID()).build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        log.debug("Updating a customer...");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
+    }
+}
